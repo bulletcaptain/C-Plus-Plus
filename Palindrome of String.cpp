1534cc0b8ca81467e1b5a5586f1b99be51c36c18
zshanoo <85954152+zshanoo@users.noreply.github.com>
2021-07-25 00:27:53 +0800

    Added on comments in the source code
    Provide guidelines to prompt users to use this feature, and explain the source code.

--- Palindrome of String.cpp
+++ Palindrome of String.cpp
@@ -6,23 +6,30 @@ using namespace std;
 int main(){
     char string1[20];
     int i, length;
+    //initialize flag to false (0)
     int flag = 0;
     
+    //prompt user to enter a string
     cout << "Enter a string: "; cin >> string1;
     
+    //get the length of the string input
     length = strlen(string1);
     
     for(i=0;i < length ;i++){
+        //perform comparision 
         if(string1[i] != string1[length-i-1]){
+            // flag set to true(1) if the characters are same
             flag = 1;
             break;
    }
 }
     
     if (flag) {
+        //print non-palindrome result if flag = 0
         cout << string1 << " is not a palindrome" << endl; 
     }    
     else {
+        //print palindrome result if flag = 1
         cout << string1 << " is a palindrome" << endl; 
     }
     
