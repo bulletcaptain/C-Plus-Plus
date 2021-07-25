2aefe3a448ad4df656612fb9289fb0bd88ace3ce
hjlew <87355982+hjlew@users.noreply.github.com>
2021-07-25 23:03:05 +0800

    Improve the code by seperate the array of eggs and floor to single variable
    In this code, the eggs and floors will not be array anymore, it is separate from each other as a single variable. This is because it will simplify the code and easier for users to use this function.

--- Dynamic Programming/Egg-Dropping-Puzzle.cpp
+++ Dynamic Programming/Egg-Dropping-Puzzle.cpp
@@ -2,44 +2,54 @@
  * in worst case with n eggs and k floors 
  */
 
-#include <iostream>
-#include <climits>
+#include <bits/stdc++.h>
 using namespace std;
-
-int eggDrop(int n, int k) {
-    int eggFloor[n+1][k+1];
-    int result;
-
-    for(int i=1; i<=n; i++) {
-        eggFloor[i][1] = 1; //n eggs..1 Floor
-        eggFloor[i][0] = 0; //n eggs..0 Floor
-    }
-    
-    // Only one egg available
-    for(int j=1; j<=k ; j++) {
-        eggFloor[1][j] = j;
-    }
-
-    for(int i=2; i<=n; i++) {
-        for(int j=2; j<=k; j++) {
-            eggFloor[i][j] = INT_MAX;
-            for(int x=1; x<=j; x++) {
-                // 1+max(eggBreak[one less egg, lower floors],
-                //       eggDoesntBreak[same # of eggs, upper floors]);
-                result = 1 + max(eggFloor[i-1][x-1], eggFloor[i][j-x]);
-                if(result < eggFloor[i][j])
-                    eggFloor[i][j] = result;
-            }
-        }
-    }
-    
-    return eggFloor[n][k];
+ 
+// A utility function to get maximum of two integers
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
 }
 
-int main() {
-    int n, k;
-    cout << "Enter number of eggs and floors: ";
-    cin>>n>>k;
-    cout << "Minimum number of trials in worst case: " << eggDrop(n, k) << endl;
+int eggDrop(int n, int k)
+{
+    // If floor = 0, trial =0
+    // If floor = 1, trial=1
+    if (k == 1 || k == 0)
+        return k;
+ 
+    //If egg = 1, k trials for k floors
+    if (n == 1)
+        return k;
+ 
+    int min = INT_MAX, x, res;
+    //Let all drop from 1st=>kth floor return min value+1
+    for (x = 1; x <= k; x++) {
+        res = max(
+            eggDrop(n - 1, x - 1),
+            eggDrop(n, k - x));
+        if (res < min)
+            min = res;
+    }
+ 
+    return min + 1;
+}
+ 
+
+int main()
+{
+    //Let n = number of eggs;
+    //Let k = number of floors
+    int n,k;
+    //Prompt for number of eggs and floors
+    cout<<"Enter number of eggs:";
+    cin>>n;
+    cout<<"Enter the number of floors:";
+    cin>>k;
+    cout << "Minimum number of trials in worst case with "
+         << n << " eggs and " 
+         << k<< " floors is "
+         << eggDrop(n, k) << endl;
     return 0;
 }
+ 
